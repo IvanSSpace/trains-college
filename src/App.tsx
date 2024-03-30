7b2ede8f9edd102e1448f95df5bdbd6b1a8c7e6b
Ivan Sharapov <oldrum75@gmail.com>
2024-03-30 16:53:14 +0100

    fix: add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import TrainsPage from './components/TrainsPage'
-import { Button } from './components/ui/button'
+import MainPage from './components/pages/MainPage'
+import Page1 from './components/pages/Page1'
+import Page2 from './components/pages/Page2'
+
 
 function App() {
 
   return (
     <>
-      <TrainsPage />
-      <Button >123</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} >
+            <Route path="/" element={<Page1 />} />
+            <Route path="/two" element={<Page2 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
