0debdc0f0395be9ced61e8d3b6abb0cedacaf926
Ivan Sharapov <oldrum75@gmail.com>
2024-03-30 15:06:52 +0100

    fix: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import TrainsPage from './components/TrainsPage'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
       <TrainsPage />
+      <Button >123</Button>
     </>
   )
 }
